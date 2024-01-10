1bf8254a60a9875c47f2596e8d4a0686f8a42e11
dev-jamo <eugenejamo@gmail.com>
2024-01-10 15:07:01 -0800

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""
-module for my lists (inherits form list)
-"""
+"""Defines a class MyList that inherits from list"""
 
 
 class MyList(list):
-    """
-    elements of the list int type
-    return my list and sorted list
+    """Class that inherits from list.
+
+    Args:
+        list (list): list to sort in ascending order.
     """
     def print_sorted(self):
-        # sorted method
-        # sorted(iterable[, key][, reverse])
-        print(sorted(self))
+        """Prints a list in ascending order.
+        """
+        list_ = self[:]
+        list_.sort()
+        print(list_)
